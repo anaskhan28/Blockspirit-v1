87cfb9d580ca6eb264dc7e60ff959276a1b51455
anaskhan28 <khananasatique@gmail.com>
2022-09-09 08:39:50 +0530

    transaction work

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -15,18 +15,19 @@ const Input = ({placeholder,name,type,value,handleChange}) => (
     className = "my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism" 
     />
 );
-const Welcome = () =>{
-    const{value}= useContext(TransactionContext);
-    
-    
-
-
-    const connectWallet = () =>{
-
-    }
-    const handleSubmit =() =>{
-
-    }
+const Welcome = () => {
+    const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
+  
+    const handleSubmit = (e) => {
+      const { addressTo, amount, keyword, message } = formData;
+  
+      e.preventDefault();
+  
+      if (!addressTo || !amount || !keyword || !message) return;
+  
+      sendTransaction();
+    };
+  
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start
@@ -38,10 +39,12 @@ const Welcome = () =>{
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and Sell cryptocurrencies easily on Krypto.
                     </p>
+                    {!currentAccount && (
                     <button type="button" onClick={connectWallet}
                     className = "flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     <p className="text-white text-base font-semibold">Connect Wallet</p>
                     </button>
+                    )}
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -84,10 +87,10 @@ const Welcome = () =>{
                         </div>
                     </div>
                     <div className="p-2 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism text-white">
-                    <Input placeholder = "Address To" name="addressTo" type="text" handleChange = {()=>{}}/>
-                    <Input placeholder = "Amount (ETH)" name="amount" type="number" handleChange = {()=>{}}/>
-                    <Input placeholder = "Keyword(Gif)" name="keyword" type="text" handleChange = {()=>{}}/>
-                    <Input placeholder = "Enter Message" name="message" type="text" handleChange = {()=>{}}/>
+                    <Input placeholder = "Address To" name="addressTo" type="text" handleChange = {handleChange}/>
+                    <Input placeholder = "Amount (ETH)" name="amount" type="number" handleChange = {handleChange}/>
+                    <Input placeholder = "Keyword(Gif)" name="keyword" type="text" handleChange = {handleChange}/>
+                    <Input placeholder = "Enter Message" name="message" type="text" handleChange = {handleChange}/>
                     
                     <div className="h-[1px] w-full bg-gray-400 my-2"/>
                     {false?(
