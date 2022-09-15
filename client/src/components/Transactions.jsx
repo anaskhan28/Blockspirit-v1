424d1d1328ce07ba9b29dffb5cb884026053ca3e
anaskhan28 <khananasatique@gmail.com>
2022-09-16 00:20:57 +0530

    initialize the Transaction part

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,30 @@
+import React, {useContext}from "react";
+
+import {TransactionContext} from '../context/TransactionContext';
+
+import { shortenAddress } from "../utils/shortenAddress";
+
 const Transactions = () =>{
+    const {currentAccount} = useContext(TransactionContext)
     return (
-        <h1>Transactions</h1>
+        <div className="flex w-full justify-center items-center 2xl:px-20 
+        gradient-bg-transactions ">
+        <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ?(
+            <h3 className="text-white text-3xl text-center my-2">
+            Latest Transactions
+            </h3>
+        ):(
+            <h3 className="text-white text-3xl text-center my-2">
+            Connect your account to see the latest transactions
+            </h3>
+        ) }
+        <div className="flex flex-wrap justify-center items-center mt-10">
+        {/* dummy data */}
+        </div>
+        
+        </div>
+        </div>
     )
 }
 export default Transactions;
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,6 +5,7 @@ import {BsInfoCircle} from 'react-icons/bs';
 
 import { TransactionContext } from "../context/TransactionContext";
 import {Loader} from './';
+import { shortenAddress } from "../utils/shortenAddress";
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 const Input = ({placeholder,name,type,value,handleChange}) => (
     <input placeholder={placeholder} 
@@ -79,7 +80,7 @@ const Welcome = () => {
                             </div>
                             <div>
                                 <p className="text-white font-light text-sm mt-10">
-                                    Address
+                                    {shortenAddress(currentAccount)}
                                 </p>
                                 <p className="text-white font- semibold text-lg mt-">
                                     Ethereum
