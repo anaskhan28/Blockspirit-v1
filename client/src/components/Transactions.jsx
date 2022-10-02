99d02b14f288d3320a33ae2394c4f1e4ce229e88
anaskhan28 <khananasatique@gmail.com>
2022-10-02 19:25:18 +0530

    fixed error in transaction context

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -17,7 +17,7 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
         ">
        <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
-          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+          <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
           </a>
           <a href={`https://goerli.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
